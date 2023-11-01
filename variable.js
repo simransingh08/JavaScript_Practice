3c6917f7e91d91887a138dd008518732129827ff
simransin <simransingh9876rajgir@gmail.com>
2023-11-01 16:53:39 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,11 @@
+// Variable can some information and here var is a keyword 
+// for creating a variable
+
+var firstname = "Simran";
+console.log(firstname);
+var lastname = "Singh";
+console.log(lastname);
+
+var sum;
+sum = firstname+" "+lastname;
+console.log(sum);
